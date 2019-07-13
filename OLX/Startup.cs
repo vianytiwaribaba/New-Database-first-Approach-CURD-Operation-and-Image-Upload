36ac232512259c394439300b6c46852979b3099e
vinaytiwari <vinaytiwari264@gmail.com>
2019-07-13 11:05:55 +0530

    Add project files.

---
+++ OLX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OLX.Startup))]
+namespace OLX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
